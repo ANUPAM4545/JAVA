995ebb2c57286249e38f2cddd64c152597184a9d
ANUPAM4545 <anupamsingh8095@gmail.com>
2024-10-24 19:47:30 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,15 @@
+class First{
+    public static void main(String[] args) {
+        System.out.println("hello\t pst");// \t is for tab
+        System.out.println("hello\npst1");// \n is for next line
+        // for seperating lines we use semicolon in java.
+    }
+}
+// VOID; it means which type of data the function returns.
+
+// MAIN; it is a method.
+
+// STATIC; it is used to manage memory and describe how objects are related to a class.
+
+// PUBLIC; it is used to make classes,methods, and variables accessible from other classes.
+
